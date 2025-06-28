b1917910e01441c5a7ee9fa96b101f941a1a444e
hi2suresh <hi2suresh@gmail.com>
2025-06-29 09:09:58 +1000

    added initial login test

---
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -0,0 +1,31 @@
+package com.ui.tests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.utility.BrowserUtility;
+public class LoginTest {
+
+	public static void main(String[] args) {
+		WebDriver wd = new ChromeDriver();
+		BrowserUtility browserUtility = new BrowserUtility(wd);
+		browserUtility.goToWebsite("http://www.automationpractice.pl/index.php?");
+		browserUtility.maximizeWindow();
+
+		By signInLinkLocator = By.xpath("//a[contains(text(), 'Sign in')]");
+        browserUtility.clickOn(signInLinkLocator);
+        
+        By emailTextBoxLocator = By.id("email");
+        browserUtility.enterText(emailTextBoxLocator, "padagi8671@iridales.com");
+        
+        By passwordTextBoxLocator = By.id("passwd");
+        browserUtility.enterText(passwordTextBoxLocator, "password");
+        
+        By submitLoginButtonLocator = By.id("SubmitLogin");
+        browserUtility.clickOn(submitLoginButtonLocator);
+              
+        
+	}
+
+}
